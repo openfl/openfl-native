ecf0ba35814f2b75009eaf0b676d5ba475a4f586
Joshua Granick <joshua@joshuagranick.com>
2013-10-22 21:02:58 -0700

    Guard call to hide system buttons (based on OS version)

--- templates/android/template/src/org/haxe/nme/GameActivity.java
+++ templates/android/template/src/org/haxe/nme/GameActivity.java
@@ -13,6 +13,7 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
@@ -465,12 +466,15 @@ public class GameActivity extends Activity implements SensorEventListener {
 		
 		super.onStart();
 		
-		::if WIN_FULLSCREEN::
-    	getWindow().getDecorView().setSystemUiVisibility (View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LOW_PROFILE | View.SYSTEM_UI_FLAG_FULLSCREEN);
-    	::end::
+		::if WIN_FULLSCREEN::::if (ANDROID_TARGET_SDK_VERSION >= 16)::
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+			
+			getWindow().getDecorView().setSystemUiVisibility (View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LOW_PROFILE | View.SYSTEM_UI_FLAG_FULLSCREEN);
+			
+		}
+		::end::::end::
 		
 		for (Extension extension : extensions) {
-			
 			extension.onStart ();
 			
 		}
