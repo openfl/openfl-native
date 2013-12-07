337358746ba185c6bfdf6b364b7d7677fdb0e42d
underscorediscovery <sven@underscorediscovery.com>
2013-12-07 06:51:54 -0330

    Audio Threading : Third times a charm. Switching to single thread for all music items, refactored to be properly thread safe, and isolated the code from the SoundChannel class for clarity. Unable to cause it to crash now on android no matter how quickly I mash the sounds in my debug app.

--- flash/media/SoundChannel.hx
+++ flash/media/SoundChannel.hx
@@ -9,16 +9,17 @@ import flash.Lib;
 #if !audio_thread_disabled 
 	#if cpp
 		import cpp.vm.Thread;
+		import cpp.vm.Mutex;
 	#end
 	#if neko
 		import neko.vm.Thread;
+		import neko.vm.Mutex;
 	#end
 #end
 
 
 class SoundChannel extends EventDispatcher {
-	
-	
+		
 	public var leftPeak (get, null):Float;
 	public var rightPeak (get, null):Float;
 	public var position (get, set):Float;
@@ -54,7 +55,7 @@ class SoundChannel extends EventDispatcher {
 			__incompleteList.push (this);
 			
 		}
-		
+
 	}
 	
 	
@@ -76,7 +77,9 @@ class SoundChannel extends EventDispatcher {
 	public function stop ():Void {
 		
 		#if !audio_thread_disabled
-			__background_thread_running = false;
+			if( __sound_instance != null && __sound_instance.__audio_type == flash.media.Sound.InternalAudioType.music ) {
+				_audio_state.remove(this);				
+			}
 		#end //audio_thread_disabled
 
 		lime_sound_channel_stop (__handle);
@@ -86,97 +89,79 @@ class SoundChannel extends EventDispatcher {
 	
 #if !audio_thread_disabled
 
-		var __background_thread_running : Bool = false;
-		var __background_thread_should_check : Bool = false;
-		var __background_thread : Thread;
-		var __main_thread : Thread;	
+		@:noCompletion private static var _audio_thread_running : Bool = false;
+		@:noCompletion public static var _audio_thread_is_idle : Bool = true;
+		@:noCompletion private static var _audio_message_check_complete = 1;
+		@:noCompletion public static var _audio_state : AudioThreadState;
 
-		var __audio_message_check_complete = 1;
-		var __audio_message_complete = 2;
-
-	@:noCompletion function __check_complete_background_thread() {		
+	@:noCompletion private static function __check_complete_background_thread() {		
 
 		var _last_complete = false;
 
-		while(__background_thread_running) {			
+		while(_audio_thread_running) {			
 
-				//first time we want to wait for the message, after that we don't
-			var thread_message : Dynamic = Thread.readMessage( (__main_thread == null) );
+			var thread_message : Dynamic = Thread.readMessage( false );
 
-				//the first message is always the calling thread
-			if(__main_thread == null) {
+			if(thread_message == _audio_message_check_complete) {
+					//check the status, firing events and otherwise
+				_audio_state.check_complete();
+			} //thread message == check complete
+			
+				//Now, if we are supposed to be updating 
+			if(!_audio_thread_is_idle) {
 
-				__main_thread = thread_message;
-				__background_thread_should_check = true;
+					//Update the list of channels complete status
+				_audio_state.update_complete();
 
-			} else {
+					//10 ms sleep time, 
+					//todo:should be configurable
+				Sys.sleep( 0.01 );
 
-				switch(thread_message) {
+			} else { //!idle
 
-					case __audio_message_check_complete :
-						if(_last_complete == true) {
-							__main_thread.sendMessage( __audio_message_complete );	
-						} 
+					//during idle we can sleep a bit longer
+					//for cpu saving purpose
 
-				} //switch thread message
-
-			} //if main thread != null
-
-			if(__background_thread_should_check) {				
-				_last_complete = __run_check_complete();				
+				Sys.sleep( 0.2 );
 			}
-
-				//10 ms sleep time, 
-				//should be configurable
-			Sys.sleep( 0.01 );
 
 		} //keep running
 
 			//clean up
-		__background_thread = null;
-		__background_thread_running = false;
-		__background_thread_should_check = false;
+		_audio_thread_running = false;
+		_audio_thread_is_idle = true;
 
-	} //__check_complete_background_thread
+	} //__check_complete_background_thread	
 
-	@:noCompletion function __ping_check_complete_thread() {
+	@:noCompletion private static function _audio_create_state() {
+
+		_audio_state = new AudioThreadState();
+
+		_audio_thread_running = true;
+		_audio_thread_is_idle = false;
+
+		_audio_state.main_thread = Thread.current();
+		_audio_state.audio_thread = Thread.create(__check_complete_background_thread);
+
+	} //_audio_create_state
+
+	@:noCompletion private static function _audio_thread_check( _sound_channel : SoundChannel ) {
 
 			//If no background thread right now, create one
-		if(__background_thread == null) {
+		if(_audio_state == null) {
+			_audio_create_state();
+		} //_audio_state == null
 
-			__background_thread_running = true;
-			__background_thread = Thread.create(__check_complete_background_thread);
-			__background_thread.sendMessage( Thread.current() );
+			//add it to the list
+		_audio_state.add(_sound_channel);
+			//Tell the thread to check the status
+		_audio_state.audio_thread.sendMessage( _audio_message_check_complete );
 
-			return;
-		}
+	} //_audio_thread_check
 
-		if(__background_thread_running != true) {			
-			return;
-		}
+#end //audio_thread_disabled	
 
-			//check if there are any messages from the background thread
-		var _message : Dynamic = Thread.readMessage(false);
-
-		if(_message != null) {			
-			switch (_message) {
-				case __audio_message_complete:
-
-					var complete_event = new Event (Event.SOUND_COMPLETE);
-						dispatchEvent (complete_event);
-
-					return;
-
-			} //switch message
-		} //message != null
-
-		__background_thread.sendMessage( __audio_message_check_complete );
-
-	} //__ping_check_complete_thread
-
-#end //audio_thread_disabled
-
-	@:noCompletion function __run_check_complete() {
+	@:noCompletion public function __run_check_complete() : Bool {
 
 		if (lime_sound_channel_is_complete (__handle)) {
 			
@@ -194,7 +179,7 @@ class SoundChannel extends EventDispatcher {
 
 		return false;
 
-	}
+	} //__run_check_complete
 	
 	@:noCompletion private function __checkComplete ():Bool {
 		
@@ -227,7 +212,7 @@ class SoundChannel extends EventDispatcher {
 
 				if( __sound_instance != null && __sound_instance.__audio_type == flash.media.Sound.InternalAudioType.music ) {
 
-					__ping_check_complete_thread();
+					_audio_thread_check( this );
 
 				} else { //sound doesn't thread, only music
 
@@ -334,3 +319,80 @@ class SoundChannel extends EventDispatcher {
 	
 	
 }
+
+	//This class will handle the state of the audio thread
+	//and make sure that touching the items is safe from either thread.
+
+class AudioThreadState {
+
+	public var mutex:Mutex;
+	public var channel_list : Map<SoundChannel, Bool>;
+
+	public var audio_thread : Thread;
+	public var main_thread : Thread;
+
+	public function new(){ 
+		mutex = new Mutex();
+		channel_list = new Map();
+	}
+
+	public function add( _channel:SoundChannel ) {
+		mutex.acquire();
+
+			if(!channel_list.exists(_channel)) {
+					//push into the list
+				channel_list.set(_channel, false);
+					//can never be idle when adding sounds
+				SoundChannel._audio_thread_is_idle = false;
+			} //if doesn't exist in list
+
+		mutex.release();
+	} //add
+
+	public function remove( _channel:SoundChannel ) {
+		mutex.acquire();
+
+			if(channel_list.exists(_channel)) {
+
+				channel_list.remove(_channel);			
+				if(Lambda.count(channel_list) == 0) {
+					SoundChannel._audio_thread_is_idle = true;
+				}	
+
+			} //if exists in list
+			
+		mutex.release();
+	} //remove
+
+	public function check_complete() {
+		
+		for(_channel in channel_list.keys()) {
+			var is_complete = channel_list.get(_channel);
+
+			if(is_complete) {
+
+				var complete_event = new Event (Event.SOUND_COMPLETE);
+					_channel.dispatchEvent (complete_event);
+
+				mutex.acquire();
+					channel_list.remove(_channel);
+				mutex.release();
+			} //if channel is complete
+		} //for each channel in list
+			
+	} //check complete
+
+	public function update_complete() {
+
+		mutex.acquire();
+
+				//run over the list and check if they are complete
+			for(_channel in channel_list.keys()) {
+				channel_list.set( _channel, _channel.__run_check_complete() );
+			}
+
+		mutex.release();
+
+	} //update complete
+
+} //AudioThreadState
