6fbfd88b973174e4a9ef52ceb73e43e938b1a117
Benjamin Davis <its.hip@gmail.com>
2014-02-16 18:53:33 +0000

    Add FrameLabel support

--- flash/display/MovieClip.hx
+++ flash/display/MovieClip.hx
@@ -5,11 +5,17 @@ class MovieClip extends Sprite {
 	
 	
 	public var currentFrame (get, null):Int;
+	public var currentFrameLabel (get, null):String;
+	public var currentLabel (get, null):String;
+	public var currentLabels (get, null):Array<FrameLabel>;
 	public var enabled:Bool;
 	public var framesLoaded (get, null):Int;
 	public var totalFrames (get, null):Int;
 	
 	@:noCompletion private var __currentFrame:Int;
+	@:noCompletion private var __currentFrameLabel:String;
+	@:noCompletion private var __currentLabel:String;
+	@:noCompletion private var __currentLabels:Array<FrameLabel>;
 	@:noCompletion private var __totalFrames:Int;
 	
 	
@@ -18,6 +24,9 @@ class MovieClip extends Sprite {
 		super ();
 		
 		__currentFrame = 0;
+		__currentFrameLabel = null;
+		__currentLabel = null;
+		__currentLabels = [];
 		__totalFrames = 0;
 		
 	}
@@ -80,6 +89,9 @@ class MovieClip extends Sprite {
 	
 	
 	private function get_currentFrame ():Int { return __currentFrame; }
+	private function get_currentFrameLabel ():String { return __currentFrameLabel; }
+	private function get_currentLabel ():String { return __currentLabel; }
+	private function get_currentLabels ():Array<FrameLabel> { return __currentLabels; }
 	private function get_framesLoaded ():Int { return __totalFrames; }
 	private function get_totalFrames ():Int { return __totalFrames; }
 	
