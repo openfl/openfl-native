35c514f79a31009db68a7c7ca895ce4ee4f7fe44
Joshua Granick <joshua@joshuagranick.com>
2014-04-23 04:26:24 -0700

    JNI fixes

--- openfl/utils/JNI.hx
+++ openfl/utils/JNI.hx
@@ -11,7 +11,142 @@ import haxe.io.Bytes;
 class JNI {
 	
 	
+	private static var alreadyCreated = new Map<String, Bool> ();
+	private static var base64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 	private static var initialized = false;
+	
+	
+	public static function callMember (method:Dynamic, jobject:Dynamic, a:Array<Dynamic>):Dynamic {
+		
+		switch (a.length) {
+			
+			case 0: return method (jobject);
+			case 1: return method (jobject, a[0]);
+			case 2: return method (jobject, a[0], a[1]);
+			case 3: return method (jobject, a[0], a[1], a[2]);
+			case 4: return method (jobject, a[0], a[1], a[2], a[3]);
+			case 5: return method (jobject, a[0], a[1], a[2], a[3], a[4]);
+			case 6: return method (jobject, a[0], a[1], a[2], a[3], a[4], a[5]);
+			case 7: return method (jobject, a[0], a[1], a[2], a[3], a[4], a[5], a[6]);
+			default: return null;
+			
+		}
+		
+	}
+	
+	
+	public static function callStatic (method:Dynamic, a:Array<Dynamic>):Dynamic {
+		
+		switch (a.length) {
+			
+			case 0: return method ();
+			case 1: return method (a[0]);
+			case 2: return method (a[0], a[1]);
+			case 3: return method (a[0], a[1], a[2]);
+			case 4: return method (a[0], a[1], a[2], a[3]);
+			case 5: return method (a[0], a[1], a[2], a[3], a[4]);
+			case 6: return method (a[0], a[1], a[2], a[3], a[4], a[5]);
+			case 7: return method (a[0], a[1], a[2], a[3], a[4], a[5], a[6]);
+			default: return null;
+			
+		}
+		
+	}
+	
+	
+	public static function createInterface (haxeClass:Dynamic, className:String, classDef:String):Dynamic {
+		
+		var bytes:Bytes = null;
+		
+		if (!alreadyCreated.get (className)) {
+			
+			bytes = Bytes.ofString (BaseCode.decode (classDef, base64));
+			bytes = Uncompress.run (bytes, 9);
+			alreadyCreated.set (className, true);
+			
+		}
+		
+		return null;
+		//return lime_jni_create_interface(haxeClass, className, bytes == null ? null : bytes.getData());
+		
+	}
+	
+	
+	public static function createMemberField (className:String, memberName:String, signature:String):JNIMemberField {
+		
+		init ();
+		
+		return new JNIMemberField (lime_jni_create_field (className, memberName, signature, false));
+		
+	}
+	
+	
+	public static function createMemberMethod (className:String, memberName:String, signature:String, useArray:Bool = false, quietFail:Bool = false):Dynamic {
+		
+		init ();
+		
+		className = className.split (".").join ("/");
+		var handle = lime_jni_create_method (className, memberName, signature, false, quietFail);
+		
+		if (handle == null) {
+			
+			if (quietFail) {
+				
+				return null;
+				
+			}
+			
+			throw "Could not find member function \"" + memberName + "\"";
+			
+		}
+		
+		var method = new JNIMethod (handle);
+		return method.getMemberMethod (useArray);
+		
+	}
+	
+	
+	public static function createStaticField (className:String, memberName:String, signature:String):JNIStaticField {
+		
+		init ();
+		
+		return new JNIStaticField (lime_jni_create_field (className, memberName, signature, true));
+		
+	}
+	
+	
+	public static function createStaticMethod (className:String, memberName:String, signature:String, useArray:Bool = false, quietFail:Bool = false):Dynamic {
+		
+		init ();
+		
+		className = className.split (".").join ("/");
+		var handle = lime_jni_create_method (className, memberName, signature, true, quietFail);
+		
+		if (handle == null) {
+			
+			if (quietFail) {
+				
+				return null;
+				
+			}
+			
+			throw "Could not find static function \"" + memberName + "\"";
+			
+		}
+		
+		var method = new JNIMethod (handle);
+		return method.getStaticMethod (useArray);
+		
+	}
+	
+	
+	public static function getEnv ():Dynamic {
+		
+		init ();
+		
+		return lime_jni_get_env ();
+		
+	}
 	
 	
 	private static function init ():Void {
@@ -43,67 +178,6 @@ class JNI {
 	}
 	
 	
-	public static function createMemberField (className:String, memberName:String, signature:String):JNIMemberField {
-		
-		init ();
-		
-		return new JNIMemberField (lime_jni_create_field (className, memberName, signature, false));
-		
-	}
-	
-	
-	public static function createMemberMethod (className:String, memberName:String, signature:String, useArray:Bool = false):Dynamic {
-		
-		init ();
-		
-		var method = new JNIMethod (lime_jni_create_method (className, memberName, signature, false));
-		return method.getMemberMethod (useArray);
-		
-	}
-	
-	
-	private static var alreadyCreated = new Map<String, Bool>();
-	private static var base64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-
-	public static function createInterface(haxeClass:Dynamic, className:String, classDef:String):Dynamic {
-	    var bytes:Bytes = null;
-	    if (!alreadyCreated.get(className)) {
-	        bytes = Bytes.ofString(BaseCode.decode(classDef, base64));
-	        bytes = Uncompress.run(bytes, 9);
-	        alreadyCreated.set(className, true);
-	    }
-	    return null;
-	    //return lime_jni_create_interface(haxeClass, className, bytes == null ? null : bytes.getData());
-	}
-
-	public static function createStaticField (className:String, memberName:String, signature:String):JNIStaticField {
-		
-		init ();
-		
-		return new JNIStaticField (lime_jni_create_field (className, memberName, signature, true));
-		
-	}
-	
-	
-	public static function createStaticMethod (className:String, memberName:String, signature:String, useArray:Bool = false):Dynamic {
-		
-		init ();
-		
-		var method = new JNIMethod (lime_jni_create_method (className, memberName, signature, true));
-		return method.getStaticMethod (useArray);
-		
-	}
-	
-	
-	public static function getEnv ():Dynamic {
-		
-		init ();
-		
-		return lime_jni_get_env ();
-		
-	}
-	
-	
 	
 	
 	// Native Methods
@@ -114,6 +188,8 @@ class JNI {
 	private static var lime_jni_create_field = Lib.load ("lime", "lime_jni_create_field", 4);
 	private static var lime_jni_create_method = Lib.load ("lime", "lime_jni_create_method", 5);
 	private static var lime_jni_get_env = Lib.load ("lime", "lime_jni_get_env", 0);
+	private static var lime_jni_call_member = Lib.load ("lime", "lime_jni_call_member", 3);
+	private static var lime_jni_call_static = Lib.load ("lime", "lime_jni_call_static", 2);
 	//private static var lime_jni_create_interface = Lib.load("lime", "lime_jni_create_interface", 3);
 	
 }
