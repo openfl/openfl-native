f9e5ea982fea635b73212616055a5eee81d217d7
Joshua Granick <joshua@joshuagranick.com>
2014-05-12 13:49:47 -0700

    New abstract vector implementation

--- flash/Vector.hx
+++ flash/Vector.hx
@@ -1,107 +1,233 @@
 package flash;
 
 
-@:arrayAccess abstract Vector<T>(Array<T>) from Array<T> to Array<T> {
+abstract Vector<T>(VectorData<T>) {
 	
 	
 	public var length (get, set):Int;
 	public var fixed (get, set):Bool;
 	
 	
-	public function new (?length:Int, ?fixed:Bool):Void {
+	public function new (?length:Int = 0, ?fixed:Bool = false):Void {
 		
-		this = new Array<T> ();
+		this = new VectorData<T> ();
+		this.data = new haxe.ds.Vector<T> (length);
+		this.length = length;
+		this.fixed = fixed;
 		
 	}
 	
 	
-	public function concat (?a:Array<T>):Vector<T> {
+	public function concat (?a:VectorData<T>):Vector<T> {
 		
-		return this.concat (a);
+		var vectorData = new VectorData<T> ();
+		vectorData.length = (a != null) ? this.length + a.length : this.length;
+		vectorData.fixed = false;
+		vectorData.data = new haxe.ds.Vector<T> (vectorData.length);
+
+		haxe.ds.Vector.blit (this.data, 0, vectorData.data, 0, this.length);
+
+		if (a != null) {
+			
+			haxe.ds.Vector.blit (a.data, 0, vectorData.data, this.length, a.length);
+			
+		}
+
+		return cast vectorData;
 		
 	}
 	
 	
 	public function copy ():Vector<T> {
 		
-		return this.copy ();
+		var vectorData = new VectorData<T> ();
+		vectorData.length = length;
+		vectorData.fixed = fixed;
+		vectorData.data = new haxe.ds.Vector<T> (length);
+		haxe.ds.Vector.blit (this.data, 0, vectorData.data, 0, this.length);
+		return cast vectorData;
 		
 	}
 	
 	
 	public function iterator<T> ():Iterator<T> {
 		
-		return this.iterator ();
+		// TODO: Limit if allocated more than actual length?
+
+		return this.data.toArray ().iterator ();
 		
 	}
 	
 	
 	public function join (sep:String):String {
 		
-		return this.join (sep);
+		var output = "";
+
+		for (i in 0...this.length) {
+			
+			if (i > 0) output += sep;
+			output += this.data[i];
+
+		}
+
+		return output;
 		
 	}
 	
 	
-	public inline function pop ():Null<T> {
+	public function pop ():Null<T> {
 		
-		return this.pop ();
+		if (!this.fixed) {
+			
+			if (this.length > 0) {
+				
+				this.length--;
+				return this.data[this.length];
+
+			}
+
+		}
+
+		return null;
 		
 	}
 	
 	
 	public inline function push (x:T):Int {
 		
-		return this.push (x);
+		if (!this.fixed) {
+			
+			this.length++;
+
+			if (this.data.length < this.length) {
+				
+				var data = new haxe.ds.Vector<T> (this.data.length + 10);
+				haxe.ds.Vector.blit (this.data, 0, data, 0, this.data.length);
+				this.data = data;
+
+			}
+
+			this.data[this.length - 1] = x;
+			
+		}
+
+		return this.length;
 		
 	}
 	
 	
 	public function reverse ():Void {
 		
-		this.reverse ();
+		var data = new haxe.ds.Vector<T> (this.length);
+
+		for (i in 0...this.length) {
+			
+			data[this.length - 1 - i] = this.data[i];
+
+		}
+
+		this.data = data;
 		
 	}
 	
 	
-	public inline function shift ():Null<T> {
+	public function shift ():Null<T> {
 		
-		return this.shift ();
+		if (!this.fixed && this.length > 0) {
+			
+			var value = this.data[0];
+			this.length--;
+			haxe.ds.Vector.blit (this.data, 1, this.data, 0, this.length);
+			return value;
+
+		}
+
+		return null;
 		
 	}
 	
 	
 	public inline function unshift (x:T):Void {
 		
-		this.unshift (x);
+		if (!this.fixed) {
+			
+			this.length++;
+
+			if (this.data.length < this.length) {
+				
+				var data = new haxe.ds.Vector<T> (this.length + 10);
+				haxe.ds.Vector.blit (this.data, 0, data, 1, this.data.length);
+				this.data = data;
+
+			} else {
+				
+				haxe.ds.Vector.blit (this.data, 0, this.data, 1, this.length - 1);
+
+			}
+
+			this.data[0] = x;
+
+		}
 		
 	}
 	
 	
-	public function slice (?pos:Int, ?end:Int):Vector<T> {
+	public function slice (?pos:Int = 0, ?end:Int = 0):Vector<T> {
 		
-		return this.slice (pos, end);
+		if (pos < 0) pos += this.length;
+		if (end <= 0) end += this.length;
+		if (end > this.length) end = this.length;
+		var length = end - pos;
+		if (length <= 0 || length > this.length) length = this.length;
+
+		var vectorData = new VectorData<T> ();
+		vectorData.length = end - pos;
+		vectorData.fixed = true;
+		vectorData.data = new haxe.ds.Vector<T> (length);
+		haxe.ds.Vector.blit (this.data, pos, vectorData.data, 0, length);
+		return cast vectorData;
 		
 	}
 	
 	
 	public function sort (f:T -> T -> Int):Void {
 		
-		this.sort (f);
+		var array = this.data.toArray ();
+		array.sort (f);
+		this.data = haxe.ds.Vector.fromArrayCopy (array);
 		
 	}
 	
 	
 	public function splice (pos:Int, len:Int):Vector<T> {
 		
-		return this.splice (pos, len);
+		if (pos < 0) pos += this.length;
+		if (pos + len > this.length) len = this.length - pos;
+		if (len < 0) len = 0;
+
+		var vectorData = new VectorData<T> ();
+		vectorData.length = len;
+		vectorData.fixed = false;
+		vectorData.data = new haxe.ds.Vector<T> (len);
+		haxe.ds.Vector.blit (this.data, pos, vectorData.data, 0, len);
+
+		if (len > 0) {
+			
+			this.length -= len;
+			haxe.ds.Vector.blit (this.data, pos + len, this.data, pos, this.length - pos);
+
+		}
+
+		return cast vectorData;
+		//return this.splice (pos, len);
 		
 	}
 	
 	
 	public function toString ():String {
 		
-		return this.toString ();
+		return "";
+		//return this.toString ();
 		
 	}
 	
@@ -110,7 +236,7 @@ package flash;
 		
 		for (i in from...this.length) {
 			
-			if (this[i] == x) {
+			if (this.data[i] == x) {
 				
 				return i;
 				
@@ -129,7 +255,7 @@ package flash;
 		
 		while (i >= from) {
 			
-			if (this[i] == x) return i;
+			if (this.data[i] == x) return i;
 			i--;
 			
 		}
@@ -141,16 +267,92 @@ package flash;
 	
 	public inline static function ofArray<T> (a:Array<Dynamic>):Vector<T> {
 		
-		return new Vector<T> ().concat (cast a);
+		var vectorData = new VectorData<T> ();
+		vectorData.length = a.length;
+		vectorData.fixed = true;
+		vectorData.data = haxe.ds.Vector.fromArrayCopy (a);
+		return cast vectorData;
 		
 	}
 	
 	
-	public inline static function convert<T,U> (v:Array<T>):Vector<U> {
+	public inline static function convert<T,U> (v:Vector<T>):Vector<U> {
 		
 		return cast v;
 		
 	}
+
+
+	@:arrayAccess public inline function arrayAccess (key:Int):T {
+		
+        return this.data[key];
+
+    }
+
+
+	@:arrayAccess public inline function arrayWrite (key:Int, value:T):T {
+		
+       return this.data[key] = value;
+
+    }
+
+
+	@:from static public inline function fromArray<T> (value:Array<T>):Vector<T> {
+		
+        var vectorData = new VectorData<T> ();
+        vectorData.length = value.length;
+        vectorData.fixed = true;
+        vectorData.data = haxe.ds.Vector.fromArrayCopy (value);
+        return cast vectorData;
+
+    }
+
+
+    @:to public inline function toArray<T> ():Array<T> {
+		
+        var value = new Array ();
+
+        for (i in 0...this.data.length) {
+        	
+        	value.push (this.data[i]);
+
+        }
+
+        return value;
+
+    }
+
+
+    @:from static public inline function fromHaxeVector<T> (value:haxe.ds.Vector<T>):Vector<T> {
+		
+		var vectorData = new VectorData<T> ();
+		vectorData.length = value.length;
+		vectorData.fixed = true;
+        vectorData.data = value;
+        return cast vectorData;
+
+    }
+
+
+    @:to public inline function toHaxeVector<T> ():haxe.ds.Vector<T> {
+		
+        return this.data;
+
+    }
+
+
+    @:from static public inline function fromVectorData<T> (value:VectorData<T>):Vector<T> {
+		
+        return cast value;
+
+    }
+
+
+    @:to public inline function toVectorData<T> ():VectorData<T> {
+		
+        return cast this;
+
+    }
 	
 	
 	
@@ -169,6 +371,20 @@ package flash;
 	
 	private inline function set_length (value:Int):Int {
 		
+		if (!fixed) {
+			
+			if (value > this.length) {
+				
+				var data = new haxe.ds.Vector<T> (value);
+				haxe.ds.Vector.blit (this.data, 0, data, 0, Std.int (Math.min (this.data.length, value)));
+				this.data = data;
+
+			}
+
+			this.length = value;
+
+		}
+
 		return value;
 		
 	}
@@ -176,16 +392,34 @@ package flash;
 	
 	private inline function get_fixed ():Bool {
 		
-		return false;
+		return this.fixed;
 		
 	}
 	
 	
 	private inline function set_fixed (value:Bool):Bool {
 		
-		return value;
+		return this.fixed = value;
 		
 	}
 	
 	
+}
+
+
+class VectorData<T> {
+
+
+	public var data:haxe.ds.Vector<T>;
+	public var fixed:Bool;
+	public var length:Int;
+
+
+	public function new () {
+		
+		length = 0;
+		
+	}
+
+
 }
