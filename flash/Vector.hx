4df0ab9eef40d42256e20a880281f9541394dfeb
Joshua Granick <joshua@joshuagranick.com>
2014-05-12 14:57:34 -0700

    Temporary runtime fix for C++

--- flash/Vector.hx
+++ flash/Vector.hx
@@ -1,3 +1,8 @@
+// Need to temporarily use old implementation for C++, since the abstract class
+// is causing runtime errors :(
+
+#if !cpp
+
 package flash;
 
 
@@ -453,3 +458,199 @@ class VectorDataIterator<T> {
 
 
 }
+
+#else
+
+package flash;
+
+
+@:arrayAccess abstract Vector<T>(Array<T>) from Array<T> to Array<T> {
+	
+	
+	public var length (get, set):Int;
+	public var fixed (get, set):Bool;
+	
+	
+	public function new (?length:Int, ?fixed:Bool):Void {
+		
+		this = new Array<T> ();
+		
+	}
+	
+	
+	public function concat (?a:Array<T>):Vector<T> {
+		
+		return this.concat (a);
+		
+	}
+	
+	
+	public function copy ():Vector<T> {
+		
+		return this.copy ();
+		
+	}
+	
+	
+	public function iterator<T> ():Iterator<T> {
+		
+		return this.iterator ();
+		
+	}
+	
+	
+	public function join (sep:String):String {
+		
+		return this.join (sep);
+		
+	}
+	
+	
+	public inline function pop ():Null<T> {
+		
+		return this.pop ();
+		
+	}
+	
+	
+	public inline function push (x:T):Int {
+		
+		return this.push (x);
+		
+	}
+	
+	
+	public function reverse ():Void {
+		
+		this.reverse ();
+		
+	}
+	
+	
+	public inline function shift ():Null<T> {
+		
+		return this.shift ();
+		
+	}
+	
+	
+	public inline function unshift (x:T):Void {
+		
+		this.unshift (x);
+		
+	}
+	
+	
+	public function slice (?pos:Int, ?end:Int):Vector<T> {
+		
+		return this.slice (pos, end);
+		
+	}
+	
+	
+	public function sort (f:T -> T -> Int):Void {
+		
+		this.sort (f);
+		
+	}
+	
+	
+	public function splice (pos:Int, len:Int):Vector<T> {
+		
+		return this.splice (pos, len);
+		
+	}
+	
+	
+	public function toString ():String {
+		
+		return this.toString ();
+		
+	}
+	
+	
+	public function indexOf (x:T, ?from:Int = 0):Int {
+		
+		for (i in from...this.length) {
+			
+			if (this[i] == x) {
+				
+				return i;
+				
+			}
+			
+		}
+		
+		return -1;
+		
+	}
+	
+	
+	public function lastIndexOf (x:T, ?from:Int = 0):Int {
+		
+		var i = this.length - 1;
+		
+		while (i >= from) {
+			
+			if (this[i] == x) return i;
+			i--;
+			
+		}
+		
+		return -1;
+		
+	}
+	
+	
+	public inline static function ofArray<T> (a:Array<Dynamic>):Vector<T> {
+		
+		return new Vector<T> ().concat (cast a);
+		
+	}
+	
+	
+	public inline static function convert<T,U> (v:Array<T>):Vector<U> {
+		
+		return cast v;
+		
+	}
+	
+	
+	
+	
+	// Getters & Setters
+	
+	
+	
+	
+	private inline function get_length ():Int {
+		
+		return this.length;
+		
+	}
+	
+	
+	private inline function set_length (value:Int):Int {
+		
+		return value;
+		
+	}
+	
+	
+	private inline function get_fixed ():Bool {
+		
+		return false;
+		
+	}
+	
+	
+	private inline function set_fixed (value:Bool):Bool {
+		
+		return value;
+		
+	}
+	
+	
+}
+
+#end
